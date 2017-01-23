0f8196d169708ef7ed25ed5787412f951e0bdf04
itumira <miradofitiavana@gmail.com>
2017-01-23 09:02:25 +0300

    third commit

--- src/Essai.java
+++ src/Essai.java
@@ -1,14 +1,16 @@
 
+import java.sql.Time;
+import metier.GestionDate;
 
 public class Essai {
 
     public static void main(String[] Args) {
-//        GestionDate gd = new GestionDate();
-//
-//        String temps = "06:00";
-//        String t = "06:00";
-//        long a = (gd.heureSouple(t)).getTime();
-//        int aa = new Time(a).getSeconds()+(new Time(a).getMinutes()*60)+(new Time(a).getHours()*3600);
-//        System.out.println(new Time(((aa*1000)/5)-10800000));
+        GestionDate gd = new GestionDate();
+
+        String temps = "06:00";
+        String t = "06:00";
+        long a = (gd.heureSouple(t)).getTime();
+        int aa = new Time(a).getSeconds() + (new Time(a).getMinutes() * 60) + (new Time(a).getHours() * 3600);
+        System.out.println(new Time(((aa * 1000) / 5) - 10800000));
     }
 }
